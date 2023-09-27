e6ef6fb970c8bf724d3c9233f3debaed485bfd83
joaovicttorsmelo <joaovicttorsilveiramelo@gmail.com>
2023-09-26 22:44:32 -0300

    Otimizacao feita com base na POO, foi deixado a opcao de grafico de pizzas documentado, caso em algum momento seja necessário utiliza-lá

--- main.py
+++ main.py
@@ -1,50 +1,60 @@
 import pandas as pd
+import plotly.graph_objects as go
 import plotly.express as px
 import time
 
-# criando variavel para ler arquivo .csv
-tabela = pd.read_csv("cancelamentos_sample.csv")
-# removendo dados sem importância para a verificação
-tabela = tabela.drop(columns="CustomerID")
 
-# removendo usuarios com pendencia de cadastro
-tabela = tabela.dropna()
-# print(tabela.info())
+# Definindo uma classe para análise de cancelamentos
+class AnaliseCancelamento:
+    def __init__(self, csv_file):
+        # Lê o arquivo CSV e armazena os dados na tabela
+        self.tabela = pd.read_csv(csv_file)
+        self.limpar_dados()  # Executa a limpeza inicial dos dados
 
-# Pegando percentual de cancelamentos
-# print(tabela["cancelou"].value_counts())
-# print(tabela["cancelou"].value_counts(normalize=True))
+    # Método privado para limpar os dados
+    def limpar_dados(self):
+        # Remove a coluna "CustomerID"
+        self.tabela = self.tabela.drop(columns="CustomerID")
+        # Remove linhas com valores nulos
+        self.tabela = self.tabela.dropna()
+        # Remove entradas mensais, pois todos os mensais cancelaram
+        self.tabela = self.tabela[self.tabela["duracao_contrato"] != "Monthly"]
+        # Filtra entradas com menos de 5 ligações para o callcenter e menos de 20 dias de atraso
+        self.tabela = self.tabela[self.tabela["ligacoes_callcenter"] < 5]
+        self.tabela = self.tabela[self.tabela["dias_atraso"] <= 20]
 
-# Pegando percentual de duração do contrato
-# print(tabela["duracao_contrato"].value_counts())
-# print(tabela["duracao_contrato"].value_counts(normalize=True))
+    # Método para analisar cancelamentos
+    def analise_cancelamento(self):
+        print("Analise de Cancelamento:")
+        # Imprime a contagem de cancelamentos
+        print(self.tabela["cancelou"].value_counts())
+        # Imprime a porcentagem de cancelamentos
+        print(self.tabela["cancelou"].value_counts(normalize=True))
+
+    # Método para gerar histogramas
+    def gerar_histograma(self):
+        print("Gerando Histogramas:")
+        # Loop pelas colunas da tabela
+        for coluna in self.tabela.columns:
+            time.sleep(3)  # Delay para visualização
+            # Cria um histograma com a coluna e cores para cancelamentos
+            grafico = px.histogram(self.tabela, x=coluna, color="cancelou")
+            grafico.show()
+
+    # def Gerar_grafico_pizza(self):
+    #     print("Gerando Graficos de Pizza:")
+    #     # Loop pelas colunas da tabela
+    #     for coluna in self.tabela.columns:
+    #         time.sleep(3)  # Delay para visualização
+    #         # Cria um gráfico de pizza com a coluna e cores para cancelamentos
+    #         fig = px.pie(self.tabela, names=coluna, title=coluna, hole=0.3)
+    #         fig.show()
 
 
-# fazendo agrupamento de dados de duracao_contrato e cancelou
-agrupamento = tabela.groupby("duracao_contrato").mean(numeric_only=True)
-# print(agrupamento)
-
-# removendo campo mensal, pois todos mensal cancelaram
-tabela = tabela[tabela["duracao_contrato"] != "Monthly"]
-# print(tabela["duracao_contrato"].value_counts())
-# print(tabela["duracao_contrato"].value_counts(normalize=True))
-
-#retireando informações da ligacoes_callcenter
-tabela = tabela[tabela["ligacoes_callcenter"] < 5]
-tabela = tabela[tabela["dias_atraso"] <= 20]
-print(tabela["cancelou"].value_counts())
-print(tabela["cancelou"].value_counts(normalize=True))
-
-# criando graficos utilizando o plotly
-for coluna in tabela.columns:
-    time.sleep(3)
-    grafico = px.histogram(tabela, x=coluna, color="cancelou")
-
-# exibindo grafico
-    grafico.show()
-
-
-    #causas de cancelamento
-# pagamento modalidade mensal
-# atrasop na fatura maior de 20 dias
-# mais que 4 ligações para o callcenter
+if __name__ == "__main__":
+    # Cria uma instância da classe CancelamentoAnalyzer com o arquivo CSV
+    analise = AnaliseCancelamento("cancelamentos_sample.csv")
+    # Executa a análise de cancelamentos
+    analise.analise_cancelamento()
+    # Gera os histogramas
+    analise.gerar_histograma()
